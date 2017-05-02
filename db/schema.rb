945db03faf7b3797c2c5d51db2895fb45a359128
Bruno Ribeiro <brunz36@gmail.com>
2017-05-02 15:42:53 -0400

    Added email field to user table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170501204526) do
+ActiveRecord::Schema.define(version: 20170502183513) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -86,6 +86,7 @@ ActiveRecord::Schema.define(version: 20170501204526) do
     t.string   "access_token"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.string   "email"
   end
 
   add_foreign_key "favorites", "homes"
